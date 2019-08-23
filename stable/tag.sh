6472c0d9bab57ec6a30eff88b1d5c17ad798a62c
Weston Steimel <weston.steimel@gmail.com>
2019-08-23 23:53:14 +0100

    switch to GitHub docker tag action now that inspect works
    Signed-off-by: Weston Steimel <weston.steimel@gmail.com>

--- stable/tag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-VERSION=$(grep -e "ARG CMATRIX_VERSION=" stable/Dockerfile)
-VERSION=${VERSION#ARG CMATRIX_VERSION=\"}
-VERSION=${VERSION%\"}
-echo "Tagging version ${VERSION}"
-docker tag "${DOCKER_USERNAME}/cmatrix:latest" "${DOCKER_USERNAME}/cmatrix:${VERSION}"
